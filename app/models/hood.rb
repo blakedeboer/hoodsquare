ff3d932e1523abba2256ba7b5b51a636426dab11
brianneking86 <brianneking86@gmail.com>
2014-11-20 18:22:20 -0500

    added method for sf hoods

--- app/models/hood.rb
+++ app/models/hood.rb
@@ -25,6 +25,17 @@ class Hood < ActiveRecord::Base
     end
   end
 
+  # makes an api call with the foursquare api and parses it - can get the name of the hood and the latitude and longitudes and other info
+  def self.sfhoods
+    html = open("https://api.foursquare.com/v2/venues/search?client_secret=4UPEJ0UD33I0UC2SX2IE3FOHOCERWWVMIIINPH1CET1UCZHY&client_id=GOQ00FEJWU4XVJVSH1W0CBRUVTIJQYIBRXBXDF2K4N1UOIG0&limit=50&venuePhotos=1&v=20140327&near=San%20Francisco,CA&radius=40000&categoryId=4f2a25ac4b909258e854f55f")
+    @file = File.read(html)
+    hood_hsh = JSON.parse(@file)
+    sf = City.find_by(:name => "San Francisco")
+    hood_hsh["response"]["venues"].each do |venue|
+      sf.hoods.create(:name => venue["name"], :latlng => "#{venue["location"]["lat"]}, #{venue["location"]["lng"]}")
+    end
+  end
+
 
   # def self.trial_explore
   #   html = open("https://api.foursquare.com/v2/venues/search?client_secret=4UPEJ0UD33I0UC2SX2IE3FOHOCERWWVMIIINPH1CET1UCZHY&client_id=GOQ00FEJWU4XVJVSH1W0CBRUVTIJQYIBRXBXDF2K4N1UOIG0&limit=50&v=20140327&&near=Soho,%20NY")
