c0d30c8e0f34d3e5ab5e9c2e17a4cb5fdbaa05bd
brianneking86 <brianneking86@gmail.com>
2014-11-21 15:53:50 -0500

    finalized hood methods to persist categories to db

--- app/models/hood.rb
+++ app/models/hood.rb
@@ -19,23 +19,13 @@ class Hood < ActiveRecord::Base
 
   # returns a hash of all venues in hood: {venue id => [venue name, venue address]}
   # SoHo radius - 250
-  def self.venue_names_addresses(hood, radius, limit)
+  def venue_names_addresses(radius, limit)
     venues ={}
-    binding.pry
-    results = Hood.foursquare.explore_venues(:ll => "#{hood.latlng}", :radius => "#{radius}", :limit => "#{limit}")
+    results = self.class.foursquare.explore_venues(:ll => "#{self.latlng}", :radius => "#{radius}", :limit => "#{limit}")
     results["groups"][0]["items"].each do |v|
       venues[v.venue.id] = [v.venue.name, v.venue.location.address]
     end
     venues
-  end
-
-  #returns all venue ids for a neighbodhood given a radius(m) and limit
-  def hood_venues(radius, limit)
-    results = self.class.foursquare.explore_venues(:ll => "#{self.latlng}", :radius => "#{radius}", :limit => "#{limit}")
-    id_array = results["groups"][0]["items"].map {|v| v.venue.id}
-    id_array.map do |venue| 
-      Hood.venue_cat_checkins(venue)
-    end
   end
 
   #returns hash of venue id, name, checkins
@@ -52,16 +42,16 @@ class Hood < ActiveRecord::Base
   def all_categories
     html = open("https://api.foursquare.com/v2/venues/categories?client_secret=4UPEJ0UD33I0UC2SX2IE3FOHOCERWWVMIIINPH1CET1UCZHY&client_id=GOQ00FEJWU4XVJVSH1W0CBRUVTIJQYIBRXBXDF2K4N1UOIG0&v=20140327&categoryId=4f2a25ac4b909258e854f55f")
     @file = File.read(html)
-    hood_hsh = JSON.parse(@file)
+    category_hsh = JSON.parse(@file)
   end
 
   #returns an array of all category names
   def all_cat_names
-    names = Hood.all_categories["response"]["categories"].map do |cat|
+    names = all_categories["response"]["categories"].map do |cat|
       cat["categories"].map do |subc|
         if subc["categories"].count > 0
-        array = subc["categories"].map {|ssc| ssc["name"]}
-        [subc["name"], array]
+          array = subc["categories"].map {|ssc| ssc["name"]}
+          [subc["name"], array]
         else 
           subc["name"]
         end
@@ -73,7 +63,7 @@ class Hood < ActiveRecord::Base
   #returns only 1st or 2nd level category
   def parent_cat(cat_id)
     category_id = ""
-    names = Hood.all_categories["response"]["categories"].each do |cat|
+    names = all_categories["response"]["categories"].each do |cat|
       if cat["id"] == cat_id
         category_id = cat["id"]
       elsif
@@ -97,11 +87,43 @@ class Hood < ActiveRecord::Base
   def venue_cat_checkins(venue_id)
     venue = self.class.foursquare.venue(venue_id)
     info = {}
-    parent_id = Hood.parent_cat(venue.categories.first.id)
+    parent_id = parent_cat(venue.categories.first.id)
     info[parent_id] = venue.stats.checkinsCount
     info
   end
 
-end
+  #returns all venue ids for a neighbodhood given a radius(m) and limit
+  def hood_categories(radius, limit)
+    results = self.class.foursquare.explore_venues(:ll => "#{self.latlng}", :radius => "#{radius}", :limit => "#{limit}")
+    id_array = results["groups"][0]["items"].map {|v| v.venue.id}
+    id_array.map do |venue| 
+      venue_cat_checkins(venue)
+    end
+  end
+
+  # returns consolidated list of categories with total checkins across those 
+  def hood_checkins
+    cat_checkins = {}
+    hood_cats = hood_categories(250, 100)
+    hood_cats.each do |cat_hsh|
+      cat = cat_hsh.keys.first
+      checkin = cat_hsh.values.first
+      if cat_checkins[cat]
+        cat_checkins[cat] += checkin
+      else
+        cat_checkins[cat] = checkin
+      end
+    end
+    cat_checkins
+  end
+
+  #creates instances of Category in database for a specific hood
+  def create_hood_cats
+    hood_checkins.each do |cat, checkins|
+      self.categories.create(:cat_id => cat, :checkins => checkins)
+    end
+  end
+
+end #end of class
 
 
