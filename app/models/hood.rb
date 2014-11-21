e51f3c5e73b7a03f4851438f19b064600e45191e
blakedeboer <deboer.blake@gmail.com>
2014-11-21 09:50:16 -0500

    added venue/category methods

--- app/models/hood.rb
+++ app/models/hood.rb
@@ -21,11 +21,55 @@ class Hood < ActiveRecord::Base
     hood_hsh = JSON.parse(@file)
   end
 
-  # def self.trial_explore
-  #   html = open("https://api.foursquare.com/v2/venues/search?client_secret=4UPEJ0UD33I0UC2SX2IE3FOHOCERWWVMIIINPH1CET1UCZHY&client_id=GOQ00FEJWU4XVJVSH1W0CBRUVTIJQYIBRXBXDF2K4N1UOIG0&limit=50&v=20140327&&near=Soho,%20NY")
-  #   @file = File.read(html)
-  #   JSON.parse(@file)
-  # end
+  # returns a hash of all venues in hood: {venue id => [venue name, venue address]}
+  # SoHo radius - 250
+  def self.venue_names_addresses(radius, limit)
+    venues ={}
+    results = @client.explore_venues(:ll => '40.7231,-74.0008', :radius => "#{radius}", :limit => "#{limit}")
+    results["groups"][0]["items"].each do |v|
+      venues[v.venue.id] = [v.venue.name, v.venue.location.address]
+    end
+    venues
+  end
+
+  #returns all venue ids for a neighbodhood given a radius(m) and limit
+  def self.venue_ids(radius, limit)
+    results = @client.explore_venues(:ll => '40.7231,-74.0008', :radius => "#{radius}", :limit => "#{limit}")
+    results["groups"][0]["items"].map {|v| v.venue.id}
+  end
+
+  def self.venue_info(venue_id)
+    venue = @client.venue(venue_id)
+    info = {}
+    info[:category_id] = venue.categories.first.id
+    info[:category_name] = venue.categories.first.name
+    info[:total_checkins] = venue.stats.checkinsCount
+    info
+  end
+
+  #returns nested hashes of all categories
+  def self.all_categories
+    html = open("https://api.foursquare.com/v2/venues/categories?client_secret=4UPEJ0UD33I0UC2SX2IE3FOHOCERWWVMIIINPH1CET1UCZHY&client_id=GOQ00FEJWU4XVJVSH1W0CBRUVTIJQYIBRXBXDF2K4N1UOIG0&v=20140327&categoryId=4f2a25ac4b909258e854f55f")
+    @file = File.read(html)
+    hood_hsh = JSON.parse(@file)
+  end
+
+  #returns an array of all category names
+  def self.all_cat_names
+    names = Hood.all_categories["response"]["categories"].map do |cat|
+      cat["categories"].map do |subc|
+        if subc["categories"].count > 0
+        array = subc["categories"].map {|ssc| ssc["name"]}
+        [subc["name"], array]
+        else 
+          subc["name"]
+        end
+      end
+    end
+    names.flatten
+  end
+
+
 
 end
 
