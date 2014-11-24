82d63da7f6430ba5a887895ec4d88eab1bfc2c66
sofia ma <sofia.ma@gmail.com>
2014-11-24 17:24:44 -0500

    conflicts fixed

--- app/controllers/hoods_controller.rb
+++ app/controllers/hoods_controller.rb
@@ -8,9 +8,9 @@ class HoodsController < ApplicationController
   def show
     @city = City.find(params[:city_id])
     @hood = Hood.find(params[:id])
-    @match_mission = Hood.find(51)
-    @match_soma = Hood.find(52)
-    @match_castro = Hood.find(60)
+    @match_mission = Hood.find(22)
+    @match_soma = Hood.find(23)
+    @match_castro = Hood.find(28)
     @matches = [@match_mission, @match_soma, @match_castro]
     @sf_hoods = City.find(2).hoods
   end
