4b671da78320385180ebd2452cf9e4d4e850aa17
sofia ma <sofia.ma@gmail.com>
2014-11-25 02:30:08 -0500

    fixed to city dropdown

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -2,7 +2,14 @@ $(function(){
   
   $('.from-city').change(function(){
     var city_id = $('.from-city').val();
-
+    
+    // repopulates the to-city dropdown list so that the city selected can be removed after selecting hood
+    $(this).children().each(function(){
+      $('.to-city option:not(:first)').remove();
+      var options = $(this).parent().children().slice(1).clone();
+      $('.to-city').append(options);
+    });
+    
     // fire ajax to go to /cities/city_id/hoods
     $.ajax('/cities/' + city_id + '/hoods', {
       type: 'get',
@@ -13,13 +20,8 @@ $(function(){
       $('.from-hood option:not(:first)').remove();
       $('.from-hood').removeAttr('disabled');
       
-      // populates the to-city dropdown list by removing the from-city that was selected
-      $('.to-city option:not(:first)').each(function(){
-        $(this).remove();
-        $('.to-city').append($('<option></option>').val($(this).val()).html($(this).text()));
-      });
-
-      $('.to-city option[value="' + city_id + '"]').remove();
+      // removes the from-city that was selected
+      $('.to-city').find('option[value="' + city_id + '"]').remove();
 
       //populates the hoods in the dropdown that match the from-city
       result.forEach(function(hood){
